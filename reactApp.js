386294b90f0421e32aa1732e68dfdbe63de1743c
Andrew Brown <DaVinci0137@gmail.com>
2020-08-11 16:52:57 -0400

    Completed Normal Mode

--- reactApp.js
+++ reactApp.js
@@ -3,8 +3,30 @@ class Team extends React.Component {
         super(props)
 
         this.state = {
-
+            shots: 0,
+            score: 0
         }
+
+        this.shotSound = new Audio("./assets/audio/Explosion+5.wav")
+        this.scoreSound = new Audio("./assets/audio/umgawa.wav")
+
+    }
+
+    shotHandler = () => {
+        let score = this.state.score
+        this.shotSound.play()
+
+        if (Math.random() > 0.5) {
+            score += 1
+
+            setTimeout(() => {
+                this.scoreSound.play()
+            }, 100)
+        }
+
+        this.setState((state, props) => ({
+            shots: state.shots + 1, score
+        }))
     }
 
     render() {
@@ -15,6 +37,16 @@ class Team extends React.Component {
                 <div className="Identity">
                     <img src={this.props.logo} alt={this.props.name} />
                 </div>
+
+                <div>
+                    <strong>Shots:</strong> {this.state.shots}
+                </div>
+
+                <div>
+                    <strong>Score:</strong> {this.state.score}
+                </div>
+
+                <button onClick={this.shotHandler}>Shoot!</button>
             </div>
         )
     }
@@ -23,7 +55,15 @@ class Team extends React.Component {
 function App(props) {
     return (
         <div className="App">
-            <Team name="Jonestown Pirates" logo="./assets/teamOneShip.gif" />
+            <div className="Stats">
+                <Team name="Jonestown Pirates" logo="./assets/teamOneShip.gif" />
+
+                <div className="versus">
+                    <h1>VS</h1>
+                </div>
+
+                <Team name="New England Redcoats" logo="./assets/teamTwoCoat.gif" />
+            </div>
         </div>
     )
 }
